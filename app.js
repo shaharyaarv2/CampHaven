d0fee155d63f15460892b2e04c5062a58f3b5809
shaharyaarv2 <shaharyaarzayer@gmail.com>
2024-09-03 15:11:41 +0000

    nothing much new added

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
 const express = require('express');
-const app = express();
-const path = require('path')
-const mongoose = require('mongoose')
-const Campground = require('./models/campground');
+const path = require('path');
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const campground = require('./models/campground');
+const Campground = require('./models/campground');
 
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp' , {});
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp' , {
-});
-
-app.use(express.urlencoded({extended: true}))
 
 const db = mongoose.connection;
-db.on('error' , console.error.bind(console , "Connection Error"))
-db.once("open" , () =>{
-    console.log("DataBase Connected !!!");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
+const app = express();
 
-app.set('view engine' , 'ejs');
-app.set('views' , path.join(__dirname, 'views'))
 
-app.get('/' , (req , res) => {
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
     res.render('home')
-})
-
-app.get('/' , (req , res ) => {
-    res.send("Hello from YelpCamp");
-})
+});
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 });
-
-app.get('/campgrounds/new' , async (req, res) =>{
-    res.render('campgrounds/new')
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
 })
-app.post('/campgrounds' , async(req, res) =>{
+
+app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', { campground })
+app.get('/campgrounds/:id', async (req, res,) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
 });
-
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
@@ -59,14 +55,15 @@ app.put('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 });
- 
-app.delete('/campgrounds/:id' , async (req , res ) => {
-    const {id} = req.params;
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    res.render('/campgrounds');
+    res.redirect('/campgrounds');
 })
 
 
+
 app.listen(3000, () => {
-    console.log("Serving on Port 3000!!")
-});
+    console.log('Serving on port 3000')
+})
--- seeds/index.js
+++ seeds/index.js
@@ -1,33 +1,30 @@
-const mongoose = require('mongoose')
-const Campground =     require('../models/campground');
-const campground = require('../models/campground');
-const {descriptors , places} = require('./seedHelpers')
+const mongoose = require('mongoose');
 const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp' , {});
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp' , {
-});
 
 const db = mongoose.connection;
-db.on('error' , console.error.bind(console , "Connection Error"))
-db.once("open" , () =>{
-    console.log("DataBase Connected !!!");
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
 const seedDB = async () => {
-    await campground.deleteMany({});
-    for(let i = 0 ; i < 50 ; i++){
-        let randomcities = Math.floor(Math.random() * 1000);
-        const camp = new campground({
-            location: `${cities[randomcities].city} , ${cities[randomcities].state}`,
-            title  : ` ${sample(descriptors)} ${sample(places)}`
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         })
-        await camp.save()
+        await camp.save();
     }
 }
 
-seedDB();
-// seedDB().then(() => {
-//     mongoose.connection.close();
-// })
+seedDB()
