900393a3338a2634d939eee441f2b1185fccd7b2
shaharyaarv2 <shaharyaarzayer@gmail.com>
2024-09-04 16:59:37 +0000

    added navbar with the help of bootstrap also used ejs mate

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp' , {});
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -15,7 +20,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp' , {});
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
