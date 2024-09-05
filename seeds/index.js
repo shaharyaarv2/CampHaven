3aac9052c89f8635d24a71d1ec20fd827eccb71c
shaharyaarv2 <shaharyaarzayer@gmail.com>
2024-09-05 18:04:04 +0000

    added styles to different pages with the help of bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`, 
+            image: `https://picsum.photos/400?random=${Math.random()}`,  
+            description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolor consectetur itaque voluptatum delectus quibusdam eius voluptatem? Quod corporis necessitatibus explicabo esse, ipsam itaque soluta omnis aliquid, libero, iure aperiam.",
+            price,
         })
         await camp.save();
     }
